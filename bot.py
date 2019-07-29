8dad80be5e40f077f5cc6afdf1fcf7b28163f3e0
Trocir <50016997+officialbfl@users.noreply.github.com>
2019-07-29 23:14:55 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -418,4 +418,4 @@ async def on_message(message):
 		embed = discord.Embed(title="Command: -credits",description='**Description:** Displays the bots info \n **Cooldown:** No cooldown \n **Usage:** -credits', color= 0x546E7A)
 		await client.send_message(message.channel, embed=embed)
 
-client.run(str(os.environ.get('BOT_TOKEN')))
+client.run('BOT_TOKEN')
